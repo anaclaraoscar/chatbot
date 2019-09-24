78f3d5a2e2da20d0f5dae80498618e44211e465a
Ana Clara <anaclara.oscar@gmail.com>
2019-09-24 16:58:43 -0300

    HAL está aprendendo

--- Chatbot/robot.py
+++ Chatbot/robot.py
@@ -3,7 +3,7 @@ from chatterbot import ChatBot
 
 robot = ChatBot('HAL9000')
 
-conversa = ['Oi', 'Olá', 'Tudo bem?', 'Tudo ótimo', 'Você gosta de programar?', 'Sim, eu programo em Python']
+conversa = ['Oi', 'Olá', 'Tudo bem?', 'Tudo ótimo', 'Você gosta de programar?', 'Sim, eu programo em Python', 'Você me entende, HAL?', 'Sim, Dave.', 'Qual o problema, HAL?', 'Acredito que você saiba qual o problema tão bem quanto eu.']
 
 trainer = ListTrainer(robot)
 trainer.train(conversa)
@@ -14,4 +14,4 @@ while True:
     if float(resposta.confidence) > 0.5:
         print('HAL9000: ', resposta)
     else:
-        print('HAL9000: Desculpe, Dave, não posso deixar que isso aconteça.')
+        print('HAL9000: Desculpe, Dave. Receio que não posso deixar que isso aconteça.')
