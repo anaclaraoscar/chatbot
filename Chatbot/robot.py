7f060153bfe6eaa0de92e1c4a1f285cc97f88e6e
Ana Clara <anaclara.oscar@gmail.com>
2019-09-25 11:43:03 -0300

    Corpus Trainer

--- Chatbot/robot.py
+++ Chatbot/robot.py
@@ -1,15 +1,19 @@
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot import ChatBot
 
-robot = ChatBot('HAL9000')
+robot = ChatBot('HAL9000', logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter',
+        'chatterbot.logic.BestMatch'
+    ])
 
-conversa = ['Oi', 'Olá', 'Tudo bem?', 'Tudo ótimo', 'Você gosta de programar?', 'Sim, eu programo em Python', 'Você me entende, HAL?', 'Sim, Dave.', 'Qual o problema, HAL?', 'Acredito que você saiba qual o problema tão bem quanto eu.']
+# conversa = ['Oi', 'Olá', 'Tudo bem?', 'Tudo ótimo', 'Você gosta de programar?', 'Sim, eu programo em Python', 'Você me entende, HAL?', 'Sim, Dave.', 'Qual o problema, HAL?', 'Acredito que você saiba qual o problema tão bem quanto eu.']
 
-trainer = ListTrainer(robot)
-trainer.train(conversa)
+trainer = ChatterBotCorpusTrainer(robot)
+trainer.train('chatterbot.corpus.english', 'chatterbot.corpus.portuguese')
 
 while True:
-    pergunta = input("Usuário: ")
+    pergunta = input("User: ")
     resposta = robot.get_response(pergunta)
     if float(resposta.confidence) > 0.5:
         print('HAL9000: ', resposta)
