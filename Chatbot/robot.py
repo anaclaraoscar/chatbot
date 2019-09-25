66b27c0d7813927bb7d5cb3a64b1f7743efcc933
Ana Clara <anaclara.oscar@gmail.com>
2019-09-25 15:01:09 -0300

    Corpus and learning

--- Chatbot/robot.py
+++ Chatbot/robot.py
@@ -3,14 +3,13 @@ from chatterbot import ChatBot
 
 robot = ChatBot('HAL9000', logic_adapters=[
         'chatterbot.logic.MathematicalEvaluation',
-        'chatterbot.logic.TimeLogicAdapter',
         'chatterbot.logic.BestMatch'
     ])
 
 # conversa = ['Oi', 'Olá', 'Tudo bem?', 'Tudo ótimo', 'Você gosta de programar?', 'Sim, eu programo em Python', 'Você me entende, HAL?', 'Sim, Dave.', 'Qual o problema, HAL?', 'Acredito que você saiba qual o problema tão bem quanto eu.']
 
 trainer = ChatterBotCorpusTrainer(robot)
-trainer.train('chatterbot.corpus.english', 'chatterbot.corpus.portuguese')
+trainer.train('chatterbot.corpus.english', 'chatterbot.corpus.Portuguese')
 
 while True:
     pergunta = input("User: ")
