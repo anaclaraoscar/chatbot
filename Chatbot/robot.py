93fa40ad2b8ae9cbd3d33c48fd1a9b9a24bfad62
Ana Clara <anaclara.oscar@gmail.com>
2019-09-27 16:12:06 -0300

    wikipedia search

--- Chatbot/robot.py
+++ Chatbot/robot.py
@@ -1,12 +1,11 @@
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot import ChatBot
+import wikipedia
 
 robot = ChatBot('HAL9000', logic_adapters=[
         'chatterbot.logic.MathematicalEvaluation',
         'chatterbot.logic.BestMatch'
     ])
-
-# conversa = ['Oi', 'Olá', 'Tudo bem?', 'Tudo ótimo', 'Você gosta de programar?', 'Sim, eu programo em Python', 'Você me entende, HAL?', 'Sim, Dave.', 'Qual o problema, HAL?', 'Acredito que você saiba qual o problema tão bem quanto eu.']
 
 trainer = ChatterBotCorpusTrainer(robot)
 trainer.train('chatterbot.corpus.english', 'chatterbot.corpus.portuguese', 'chatterbot.corpus.custom.HAL')
@@ -14,7 +13,18 @@ trainer.train('chatterbot.corpus.english', 'chatterbot.corpus.portuguese', 'chat
 while True:
     pergunta = input('User: ')
     resposta = robot.get_response(pergunta)
+    wiki = wikipedia.search(pergunta)
     if float(resposta.confidence) > 0.5:
-        print('HAL9000: ', resposta)
+        print('HAL9000: ', resposta) 
+    elif pergunta in ('wikipedia', 'search'):
+        print('HAL9000: Here is your search', wiki)
     else:
         print('HAL9000: Dave, this conversation can serve no purpose anymore. Goodbye.')
+
+# while True:
+#     pesquisa = input('Hal, search this: ', pergunta)
+#     wiki = wikipedia.search(pergunta)
+#     if float(pesquisa.confidence) > 0.5:
+#         print('HAL9000: Here is your answer', wiki)
+#     else:
+#         print('HAL9000: Dave, this conversation can serve no purpose anymore. Goodbye.')
